e14a1efe8b81f95fa048e65a39f0295075abe57c
Saleh Zaidan <salehzaidan53@gmail.com>
2020-07-05 22:18:37 +0700

    Menambahkan style custom

--- js/script.js
+++ js/script.js
@@ -4,3 +4,14 @@ const btn = document.querySelector("nav .btn");
 btn.addEventListener("click", () => {
   btn.innerHTML = "Ucup Surucup";
 });
+
+// Pindah bagian aktif
+const items = document.querySelectorAll(".nav-item");
+items.forEach((item) => {
+  item.addEventListener("click", () => {
+    items.forEach((e) => {
+      e.classList.remove("active");
+    });
+    item.classList.add("active");
+  });
+});
